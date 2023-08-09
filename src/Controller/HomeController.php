9ce9c4752eed454aa9994ebe88a708f85cc487bb
akachafatma1@gmail.com <akachafatma1@gmail.com>
2023-08-09 03:30:46 +0100

    login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,19 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArticleRepository;
+use Symfony\Component\Security\Core\Security; // Import the Security class
 
 class HomeController extends AbstractController
 {
     private $articleRepo;
+    private $security; // Add a property to hold the Security instance
 
-    public function __construct(ArticleRepository $articleRepo)
+    public function __construct(ArticleRepository $articleRepo, Security $security)
     {
         $this->articleRepo = $articleRepo;
+        $this->security = $security; // Inject the Security instance
     }
 
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        // Check if the user is not authenticated
+        if (!$this->security->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_login'); // Redirect to the login page
+        }
+
         $articles = $this->articleRepo->findAll();
 
         // Convert the articles to an array with the required data (e.g., name and quantity)
@@ -35,4 +43,3 @@ class HomeController extends AbstractController
         ]);
     }
 }
-
